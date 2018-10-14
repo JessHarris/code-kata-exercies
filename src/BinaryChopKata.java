5ac8fe87940f41e8a7e91a6b5eb1ffd763bb2ae9
JessHarris <jess.harristn@gmail.com>
2018-10-14 09:16:12 -0500

    binary chop kata

--- src/BinaryChopKata.java
+++ src/BinaryChopKata.java
@@ -2,23 +2,17 @@ import java.util.Arrays;
 
 public class BinaryChopKata {
     public static void main (String [] args) {
-        int [] array = {0,1,2};
+        int [] array = {14, 6, 23, 18, 9, 2, 10};
         Arrays.sort(array);
         System.out.println(Arrays.toString(array));
 
-        int numToFind = 2;
+        int numToFind = 6;
         int iterative = binarySearchWithIterative(numToFind, array);
         System.out.println("Iterative approach: " + numToFind + " is at " + iterative + " index");
 
-        numToFind = 3;
+        numToFind = 10;
         int recursive = binarySearchWithRecursion(numToFind, array, 0, array.length-1);
         System.out.println("Recursive approach: " + numToFind + " is at " + recursive + " index");
-
-//        numToFind = 2;
-//        int arraySlicing = binarySearchWithArraySlicing(numToFind, array);
-//        System.out.println("Array Slicing approach: " + numToFind + " is at " + arraySlicing + " index");
-
-
     }
     public static int binarySearchWithIterative(int x, int [] array){
         int low = 0;
@@ -59,28 +53,4 @@ public class BinaryChopKata {
         }
         return -1;
     }
-
-//    public static int binarySearchWithArraySlicing(int x,int [] array){
-//        int low = 0;
-//        int high = array.length -1;
-//
-//        while(low<=high){
-//            int middle = (high+low) / 2;
-//
-//            if(array[middle] == x){
-//                return middle;
-//            }
-//            else if(array[middle] < x){
-//                low = middle + 1;
-//                array = Arrays.copyOfRange(array, low, high);
-//                return binarySearchWithArraySlicing(x,array);
-//            }
-//            else{
-//                high = middle - 1;
-//                array = Arrays.copyOfRange(array, low, high);
-//                return binarySearchWithArraySlicing(x, array);
-//            }
-//        }
-//        return -1;
-//    }
 }
